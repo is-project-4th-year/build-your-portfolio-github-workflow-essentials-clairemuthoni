9acdd6097de7accfb772f24dda1487c79589ebc0
Claire Muthoni <muthoniclaire664@gmail.com>
2025-06-24 21:07:23 +0300

    Boilerplate for a React project with Tailwind CSS and PostCSS

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return(
+    <div className="bg-green-500 text-white p-10 text-3xl font-bold">
+      Tailwind CSS v3 is working !
+    </div>
+  );
+}
+
+export default App;
